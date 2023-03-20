67be0c0cfc73736b5a916743a461a3cd5c202e15
Dexter Flores <120296202+dexterflores200@users.noreply.github.com>
2023-03-20 16:33:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
+const middlename = document.getElementById('middlename');
+const gender = document.getElementById('gender');
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    Validate();
+})
+
+const sendData = (usernameVal, sRate, Count) => {
+    if(sRate === Count){
+        swal("Hello " + usernameVal , "You are Registered", "success");
+    }
+}
+
+const SuccessMsg = (usernameVal) => {
+    let formContr = document.getElementsByClassName('form-control');
+    var Count = formContr.length - 1;
+    for(var i = 0; i < formContr.length; i++){
+        if(formContr[i].className === "form-control success"){
+            var sRate = 0 + i;
+            console.log(sRate);
+            sendData(usernameVal, sRate, Count);
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+
+const isEmail = (emailVal) =>{
+    var atSymbol = emailVal.indexOf('@');
+    if(atSymbol < 1) return false;
+    var dot = emailVal.lastIndexOf('.');
+    if(dot <= atSymbol + 2) return false;
+    if(dot === emailVal.length -1) return false;
+    return true;
+}
+
+function Validate(){
+    const usernameVal = username.value.trim();
+    const lastnameVal = lastname.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+    const middlenameVal = middlename.value.trim();
+    const genderVal = gender.value.trim();
+    
+
+    //username
+    if(usernameVal === ""){
+        setErrorMsg(username, 'first name cannot be blank');
+    }
+    else if(usernameVal.length <=1){
+        setErrorMsg(username, 'min 2 char');
+    }
+    else{
+        setSuccessMsg(username);
+    }
+
+    //last name
+
+    if(lastnameVal === ""){
+        setErrorMsg(lastname, 'last name cannot be blank');
+    }
+    else if(lastnameVal.length <=1){
+        setErrorMsg(lastname, 'min 2 char');
+    
+    else{
+        setSuccessMsg(lastname);
+    }
+
+    //email
+    if(emailVal === ""){
+        setErrorMsg(email, 'email cannot be blank');
+    }
+    else if(!isEmail(emailVal)){
+        setErrorMsg(email, 'email is not valid');
+    }
+    else{
+        setSuccessMsg(email);
+    }
+
+    //password
+    if(passwordVal === ""){
+        setErrorMsg(password, 'password cannot be blank');
+    }
+    else if(passwordVal.length <= 7){
+        setErrorMsg(password, 'min 8 char');
+    }
+    else{
+        setSuccessMsg(password);
+    }
+
+    //confirm password
+    if(cpasswordVal === ""){
+        setErrorMsg(cpassword, 'confirm password cannot be blank');
+    }
+    else if(passwordVal != cpasswordVal){
+        setErrorMsg(cpassword, 'Not Matched!');
+    }
+    else{
+        setSuccessMsg(cpassword);
+    }
+    SuccessMsg(usernameVal);
+
+   //middle name
+
+
+    if(middlenameVal === ""){
+        setErrorMsg(middlename, 'last name cannot be blank');
+    }
+    else if(middlenameVal.length <=1){
+        setErrorMsg(middlename, 'min 2 char');
+    }
+    else{
+        setSuccessMsg(middlename);
+    }
+
+//gender
+
+
+    if(genderVal === ""){
+        setErrorMsg(gender, 'select gender');
+    }
+    
+    
+    else{
+        setSuccessMsg(gender);
+    }
+
+
+
+}
+
+function setErrorMsg(input, errormsgs){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = "form-control error";
+    small.innerText = errormsgs;
+}
+
+function setSuccessMsg(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
